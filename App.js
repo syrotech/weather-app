109399b3cb190c338933ba6c2ba21843057b006f
jaemcu <cupp.j.e@gmail.com>
2020-02-23 20:28:39 -0500

    Added "isLoading" state text in 'App.js', created 'Components' folder
    1. Added "isLoading" state text to 'App.js'
    2. Changed state to 'true' for now in development
    3. Added 'components' Folder
    4. Added 'Weather.js' file for first screen
    5. Defined weather component in 'Weather.js'
    6. Added text and styling to 'Weather.js' component

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+export default class App extends React.Component {
+    state = {
+      isLoading: true
+    };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    render() {
+      const { isLoading } = this.state;
+      return (
+        <View style = {styles.container}>
+        { isLoading ? (
+          <Text>Fetching The Weather...</Text>
+        ) : (
+          <View>
+            <Text > Minimalist Weather App < /Text>
+            </View>
+          )}
+          </View>
+      );
+    }
+  }
+
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    });
